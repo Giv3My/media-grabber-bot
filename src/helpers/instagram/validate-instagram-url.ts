c7d748cf96b5095e8bc07ffe6806ef92eb739a7c
Giv3My <bessvlad45@gmail.com>
2023-07-27 14:24:02 +0300

    Fix instagram url validation

--- src/helpers/instagram/validate-instagram-url.ts
+++ src/helpers/instagram/validate-instagram-url.ts
@@ -1,5 +1,7 @@
 export const validateInstgramUrl = (url: string) => {
-  const reg = new RegExp('(https://www.instagram.com/(?:p|reel|tv)/[a-zA-Z0-9_-]{11})');
+  const reg = new RegExp(
+    '(https://www.instagram.com/(?:p|reel(|s)|tv)/[a-zA-Z0-9_-]{11})'
+  );
   const match = url.match(reg);
 
   return Boolean(match);
